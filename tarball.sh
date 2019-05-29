fa3bae1cecde91397f163d98f8043a89d8fa93f4
Ernst Sjöstrand <ernstp@gmail.com>
2019-05-29 17:53:22 +0200

    Cache downloaded tarballs

--- tarball.sh
+++ tarball.sh
@@ -1,18 +1,30 @@
 #!/bin/bash -ex
-
 set -o pipefail
+
+function download() {
+	url=$1
+	name=$2
+
+	mkdir -p "$DOWNLOADS"
+	if [ ! -e "$DOWNLOADS/$name" ]; then
+		curl -f "$url" -o "$DOWNLOADS/$name"
+	fi
+	cp "$DOWNLOADS/$name" "$name"
+}
 
 rm -vf *.dsc *.build *.buildinfo *.changes *.upload *.tar.gz *.tar.xz *.tar.bz2
 
 source settings.sh
 
-PACKAGE_NAME=$(basename "$PWD")
-
+BASEDIR=$(basename "$PWD")
+PACKAGE_NAME=${PACKAGE_NAME:-$BASEDIR}
+DOWNLOADS="../downloads"
 PACKAGE_VERSION=$(echo $RELEASE_VERSION | tr -- - \~)
-curl -f "${URL}/${PACKAGE_NAME}-${RELEASE_VERSION}.tar.$COMPR" \
-	-o "${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.$COMPR"
-curl -f "${URL}/${PACKAGE_NAME}-${RELEASE_VERSION}.tar.$COMPR.sig" \
-	-o "${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.$COMPR.sig"
+
+download "${URL}/${PACKAGE_NAME}-${RELEASE_VERSION}.tar.$COMPR" \
+	"${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.$COMPR"
+download "${URL}/${PACKAGE_NAME}-${RELEASE_VERSION}.tar.$COMPR.sig" \
+	"${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.$COMPR.sig"
 
 gpg --verify "${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.$COMPR.sig" \
 	"${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.$COMPR"
