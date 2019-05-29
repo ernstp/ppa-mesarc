da0b88b38f199fbe68f0386e94594922ac088e5c
Ernst Sjöstrand <ernstp@gmail.com>
2019-05-29 18:02:51 +0200

    mesa 19.1.0-rc4!
    Also drop mesa-old setup by adding proposed packages to PPA build
    dependency for llvm-8.

--- mesa-old/settings.sh
+++
@@ -1,4 +0,0 @@
-URL="https://mesa.freedesktop.org/archive/"
-COMPR="xz"
-RELEASE_VERSION="19.1.0-rc3"
-INC=0
--- mesa/settings.sh
+++ mesa/settings.sh
@@ -1,5 +1,5 @@
 URL="https://mesa.freedesktop.org/archive/"
 COMPR="xz"
-RELEASE_VERSION="19.1.0-rc3"
-INC=2
-DISTROS="disco"
+RELEASE_VERSION="19.1.0-rc4"
+INC=0
+DISTROS="bionic cosmic disco"
