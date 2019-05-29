06dbb928f86fcf51f9018a5393fb435e92fd7c62
Ernst Sjöstrand <ernstp@gmail.com>
2019-05-29 18:40:59 +0200

    Add settings.sh for git-based packages

--- git.sh
+++ git.sh
@@ -4,9 +4,12 @@ set -o pipefail
 
 rm -vf *.dsc *.build *.buildinfo *.changes *.upload *.tar.gz *.tar.xz
 
-GDIR=$(basename "$PWD")
+source settings.sh
 
-cd "$GDIR"
+BASEDIR=$(basename "$PWD")
+PACKAGE_NAME=${PACKAGE_NAME:-$BASEDIR}
+
+cd "$PACKAGE_NAME"
 
 git clean -xdf
 git pull
@@ -17,7 +20,7 @@ PACKAGE_VERSION=$(grep AC_INIT -A 1 configure.ac | grep -o -E '[0-9]+\.[0-9]+\.[
 
 GIT_VERSION=$(git show --date=format:%y%m%d%H%M -s --format=format:%cd.%h)
 
-for dist in "$@"; do
+for dist in $DISTROS; do
 	DEBIAN_VERSION=${PACKAGE_VERSION}+git${GIT_VERSION}~${dist:0:1}~mesarc0
 
 	dch --distribution ${dist} -v ${DEBIAN_VERSION} "New snapshot"
---
+++ xserver-xorg-video-amdgpu/settings.sh
@@ -0,0 +1,1 @@
+DISTROS="bionic cosmic disco"
