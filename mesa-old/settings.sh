0cf24eb6f7858a5bfbf0801ec35113460cb77dce
Ernst Sjöstrand <ernstp@gmail.com>
2019-06-14 22:36:38 +0200

    mesa 19.1.0!

--- mesa-old/settings.sh
+++ mesa-old/settings.sh
@@ -1,6 +1,6 @@
 URL="https://mesa.freedesktop.org/archive/"
 COMPR="xz"
-RELEASE_VERSION="19.1.0-rc4"
-INC=1
+RELEASE_VERSION="19.1.0"
+INC=0
 PACKAGE_NAME=mesa
 DISTROS="cosmic"
--- mesa/settings.sh
+++ mesa/settings.sh
@@ -1,5 +1,5 @@
 URL="https://mesa.freedesktop.org/archive/"
 COMPR="xz"
-RELEASE_VERSION="19.1.0-rc4"
+RELEASE_VERSION="19.1.0"
 INC=0
 DISTROS="bionic disco eoan"
