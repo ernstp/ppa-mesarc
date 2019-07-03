57a9a37506a9e50ce67520dbd1e7e61753a51ed8
Ernst Sjöstrand <ernstp@gmail.com>
2019-07-03 21:11:50 +0200

    git.sh: New features
    Use separate git dir
    INC support
    VERSION file support (meson related?)
    PREBUMP/SEPARATOR support

--- git.sh
+++ git.sh
@@ -8,9 +8,10 @@ source settings.sh
 
 BASEDIR=$(basename "$PWD")
 PACKAGE_NAME=${PACKAGE_NAME:-$BASEDIR}
+INC=${INC:-0}
 
 if [ ! -e "$PACKAGE_NAME/.git" ]; then
-	git clone "$GIT" "$PACKAGE_NAME"
+	git clone --separate-git-dir=git "$GIT" "$PACKAGE_NAME"
 fi
 cd "$PACKAGE_NAME"
 
@@ -19,12 +20,23 @@ git pull
 
 cp -r ../debian .
 
-PACKAGE_VERSION=$(grep AC_INIT -A 1 configure.ac | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')
+if [ -e VERSION ]; then
+	PACKAGE_VERSION=$(cat VERSION | sed "s/-devel//")
+else
+	PACKAGE_VERSION=$(grep AC_INIT -A 1 configure.ac | grep -o -E '[0-9]+\.[0-9]+\.[0-9]+')
+fi
+
+# Is the version number in git incresed to the next version in advance or not?
+if [ -n "$PREBUMP" ]; then
+	SEPARATOR="~"
+else
+	SEPARATOR="+"
+fi
 
 GIT_VERSION=$(git show --date=format:%y%m%d%H%M -s --format=format:%cd.%h)
 
-for dist in $DISTROS; do
-	DEBIAN_VERSION=${PACKAGE_VERSION}+git${GIT_VERSION}~${dist:0:1}~mesarc0
+for dist in $DISTROS ; do
+	DEBIAN_VERSION=${PACKAGE_VERSION}${SEPARATOR}git${GIT_VERSION}~${dist:0:1}~mesarc${INC}
 
 	dch --distribution ${dist} -v ${DEBIAN_VERSION} "New snapshot"
 
