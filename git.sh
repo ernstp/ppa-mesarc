791b6a3cc42dd624fd6ea0d92eac029a751d4845
Ernst Sjöstrand <ernstp@gmail.com>
2019-07-04 15:03:27 +0200

    git.sh: Add changelog to git packages

--- git.sh
+++ git.sh
@@ -12,10 +12,17 @@ INC=${INC:-0}
 
 if [ ! -e "$PACKAGE_NAME/.git" ]; then
 	git clone --separate-git-dir=git "$GIT" "$PACKAGE_NAME"
+	newclone=1
 fi
+
 cd "$PACKAGE_NAME"
 
+if [ -z "$newclone" ]; then
+	OLD_GIT_REV=$(git show -s --format=format:%h)
+fi
+
 git clean -xdf
+git reset --hard
 git pull
 
 cp -r ../debian .
@@ -34,11 +41,19 @@ else
 fi
 
 GIT_VERSION=$(git show --date=format:%y%m%d%H%M -s --format=format:%cd.%h)
+GIT_REV=$(git show -s --format=format:%h)
+
+if [ "x$GIT_REV" = "x$OLD_GIT_REV" ]; then
+	echo "Nothing new"
+	exit 0
+else
+	CHANGES=$(git log --oneline "${OLD_GIT_REV}..${GIT_REV}")
+fi
 
 for dist in $DISTROS ; do
 	DEBIAN_VERSION=${PACKAGE_VERSION}${SEPARATOR}git${GIT_VERSION}~${dist:0:1}~mesarc${INC}
 
-	dch --distribution ${dist} -v ${DEBIAN_VERSION} "New snapshot"
+	dch --distribution ${dist} -v ${DEBIAN_VERSION} "$CHANGES"
 
 	debuild -S -d
 done
