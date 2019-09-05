d110c94f0b6a5672dba60f82229a0994567bc1fb
Ernst Sjöstrand <ernstp@gmail.com>
2019-09-05 06:26:07 +0200

    mutter: switch to master branch

--- mutter/settings.sh
+++ mutter/settings.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME=mutter
-GIT="https://gitlab.gnome.org/vanvugt/mutter.git"
-GITBRANCH="geometric-picking"
+GIT="https://gitlab.gnome.org/GNOME/mutter.git"
+GITBRANCH="master"
 DISTROS="eoan"
-INC=7
+INC=0
 PPA=gnome
