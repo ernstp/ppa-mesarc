b5338d9793df3dcec4ebd458bc180955ebc3a94c
Ernst Sjöstrand <ernstp@gmail.com>
2020-03-11 15:11:47 +0100

    mutter-36

---
+++ mutter-36/settings.sh
@@ -0,0 +1,6 @@
+PACKAGE_NAME=mutter
+GIT="https://gitlab.gnome.org/jadahl/mutter.git"
+GITBRANCH="wip/wayland-fullscreen-unredirect"
+DISTROS="focal"
+INC=redirect0
+PPA=gnome
--- mutter/settings.sh
+++ mutter/settings.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME=mutter
 GIT="https://gitlab.gnome.org/GNOME/mutter.git"
 GITBRANCH="gnome-3-34"
-DISTROS="eoan focal"
-INC=2
+DISTROS="eoan"
+INC=0
 PPA=gnome
