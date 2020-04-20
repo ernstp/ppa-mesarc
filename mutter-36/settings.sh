e03400ace49d828c13b17418a57822dab4dfdbdd
Ernst Sjöstrand <ernstp@gmail.com>
2020-04-20 19:19:45 +0200

    mutter-git: update to 3.37

--- mutter-36/settings.sh
+++
@@ -1,6 +0,0 @@
-PACKAGE_NAME=mutter
-GIT="https://gitlab.gnome.org/jadahl/mutter.git"
-GITBRANCH="wip/wayland-fullscreen-unredirect"
-DISTROS="focal"
-INC=redirect0
-PPA=gnome
---
+++ mutter-git/settings.sh
@@ -0,0 +1,6 @@
+PACKAGE_NAME=mutter
+GIT="https://gitlab.gnome.org/GNOME/mutter.git"
+GITBRANCH="master"
+DISTROS="focal"
+INC=redirect0
+PPA=gnome
