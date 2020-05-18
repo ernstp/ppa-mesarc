70cffe2f9c1a6fdbc9229eb6c29d782ef9cb3456
Ernst Sjöstrand <ernstp@gmail.com>
2020-05-18 23:07:59 +0200

    mutter: Go back to 3.36

--- mutter-36/settings.sh
+++ mutter-36/settings.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME=mutter
-GIT="https://gitlab.gnome.org/jadahl/mutter.git"
-GITBRANCH="wip/wayland-fullscreen-unredirect"
+GIT="https://gitlab.gnome.org/GNOME/mutter.git"
+GITBRANCH="gnome-3-36"
 DISTROS="focal"
 INC=redirect0
 PPA=gnome
