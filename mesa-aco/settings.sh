3822424359032565f2f053e4c1b072ba9fe1dcd4
Ernst Sjöstrand <ernstp@gmail.com>
2020-11-25 11:54:10 +0100

    mesa: Disable lto for Bionic

--- mesa-aco/settings.sh
+++ mesa-aco/settings.sh
@@ -3,5 +3,5 @@ GIT="https://gitlab.freedesktop.org/mesa/mesa.git"
 GITBRANCH="master"
 DISTROS="bionic focal groovy"
 PREBUMP=1
-INC=6
+INC=7
 PPA=mesaaco
--- mesa-git/settings.sh
+++ mesa-git/settings.sh
@@ -2,5 +2,5 @@ PACKAGE_NAME=mesa
 GIT="https://gitlab.freedesktop.org/mesa/mesa.git"
 GITBRANCH="staging/20.3"
 DISTROS="bionic focal groovy"
-INC=1
+INC=2
 PPA=mesarc
