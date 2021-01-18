5c7d293e5d442cadfd889d1741d2b03a46866e54
Ernst Sjöstrand <ernstp@gmail.com>
2021-01-18 20:03:11 +0100

    mesa-git: 21.0 release candidates!

--- mesa-git/settings.sh
+++ mesa-git/settings.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME=mesa
 GIT="https://gitlab.freedesktop.org/mesa/mesa.git"
-GITBRANCH="staging/20.3"
+GITBRANCH="staging/21.0"
 DISTROS="bionic focal groovy hirsute"
-INC=2
+INC=0
 PPA=mesarc
