1ee32d227db626bc8c207a24fc2ba85a7204280e
Ernst Sjöstrand <ernstp@gmail.com>
2021-04-14 00:10:56 +0200

    libdrm: 2.4.105

--- libdrm/settings.sh
+++ libdrm/settings.sh
@@ -1,5 +1,5 @@
 URL="https://dri.freedesktop.org/libdrm/"
 COMPR="xz"
-RELEASE_VERSION="2.4.103"
-INC=0
-DISTROS="bionic focal groovy"
+RELEASE_VERSION="2.4.105"
+INC=1
+DISTROS="bionic focal groovy hirsute"
