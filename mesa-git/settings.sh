d164c71b69adceb920bf88d9e39e9ff08b122b46
Ernst Sjöstrand <ernstp@gmail.com>
2021-04-17 21:01:09 +0200

    mesa-git: 21.1-rc

--- mesa-git/settings.sh
+++ mesa-git/settings.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME=mesa
 GIT="https://gitlab.freedesktop.org/mesa/mesa.git"
-GITBRANCH="staging/21.0"
+GITBRANCH="staging/21.1"
 DISTROS="bionic focal groovy hirsute"
-INC=2
+INC=0
 PPA=mesarc
