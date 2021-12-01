5cf3c2e6e1735a14125fb5456b877d09df6f40b5
Ernst Sjöstrand <ernstp@gmail.com>
2021-12-01 21:25:08 +0100

    libdrm: 2.4.109

--- libdrm/settings.sh
+++ libdrm/settings.sh
@@ -1,5 +1,5 @@
 URL="https://dri.freedesktop.org/libdrm/"
 COMPR="xz"
-RELEASE_VERSION="2.4.107"
-INC=3
-DISTROS="bionic focal hirsute impish"
+RELEASE_VERSION="2.4.109"
+INC=0
+DISTROS="bionic focal hirsute impish jammy"
