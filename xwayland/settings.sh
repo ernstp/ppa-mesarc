37ccb8b803ef3bb4e505976d3439014866035fbe
Ernst Sjöstrand <ernstp@gmail.com>
2021-12-09 19:21:47 +0100

    xwayland: build from my test branch

--- xwayland/settings.sh
+++ xwayland/settings.sh
@@ -1,7 +1,7 @@
-GIT="https://gitlab.freedesktop.org/xorg/xserver.git"
+GIT="https://gitlab.freedesktop.org/ernstp/xserver.git"
 GITBRANCH="xwayland-21.1"
 PACKAGE_NAME=xwayland
 DISTROS="impish jammy"
-INC=1
+INC=2
 PPA=wayland
 EPOCH="2:"
