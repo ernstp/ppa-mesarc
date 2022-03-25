aa52d4857bc4e3397a5b0d3a6cf23de754ee2969
Ernst Sjöstrand <ernstp@gmail.com>
2022-03-25 23:38:40 +0100

    xwayland: switch to 22.1 branch

--- xwayland/settings.sh
+++ xwayland/settings.sh
@@ -1,5 +1,5 @@
-GIT="git@gitlab.freedesktop.org:xorg/xserver.git"
-GITBRANCH="master"
+GIT="https://gitlab.freedesktop.org/xorg/xserver.git"
+GITBRANCH="xwayland-22.1"
 PACKAGE_NAME=xwayland
 DISTROS="impish jammy"
 INC=5
