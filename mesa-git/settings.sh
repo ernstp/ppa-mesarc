3fb9314b77e9e2a4b1b50bd262fcb15218fb7a55
Ernst Sjöstrand <ernstp@gmail.com>
2022-11-19 22:05:50 +0100

    mesa: Drop bionic from main, add lunar!

--- mesa-git/settings.sh
+++ mesa-git/settings.sh
@@ -1,6 +1,6 @@
 PACKAGE_NAME=mesa
 GIT="https://gitlab.freedesktop.org/mesa/mesa.git"
 GITBRANCH="staging/22.3"
-DISTROS="bionic focal jammy kinetic"
+DISTROS="bionic focal jammy kinetic lunar"
 INC=3
 PPA=mesarc
