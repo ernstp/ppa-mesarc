5f6cf3976bd523e2328132e3f977e10cdba1e12f
Ernst Persson <ernstp@gmail.com>
2025-07-23 19:12:48 +0200

    xserver-xorg-video-amdgpu: convert to git2

--- xserver-xorg-video-amdgpu-hwe/settings.sh
+++
@@ -1,4 +0,0 @@
-PACKAGE_NAME="xserver-xorg-video-amdgpu-hwe-18.04"
-GIT="https://gitlab.freedesktop.org/xorg/driver/xf86-video-amdgpu.git"
-DISTROS="bionic"
-PPA=mesarc
--- xserver-xorg-video-amdgpu/settings2.sh
+++ xserver-xorg-video-amdgpu/settings2.sh
@@ -1,3 +1,3 @@
 GIT="https://gitlab.freedesktop.org/xorg/driver/xf86-video-amdgpu.git"
-DISTROS="bionic focal jammy noble oracular"
 PPA=mesarc
+INC=1
